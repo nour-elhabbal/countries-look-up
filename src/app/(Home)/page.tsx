477758dad82cb62802eaf5eb41813e577231c30e
Nour Ahmed <nour.elhabbal9@gmail.com>
2024-12-09 23:27:18 +0200

    Fix: wrapped any component that uses useSearchParams in a suspense

--- src/app/(Home)/page.tsx
+++ src/app/(Home)/page.tsx
@@ -3,25 +3,28 @@ import { Box, Flex } from "@chakra-ui/react";
 import CountriesList from "./components/CountriesList";
 import FiltersMenu from "@/components/FiltersMenu";
 import Search from "../../components/Search";
+import { Suspense } from "react";
 
 const Home = async () => {
   return (
     <>
       <Box>
-        <Flex
-          direction={["column", null, "row"]}
-          align={["start", null, "center"]}
-          justify="space-between"
-          gapY="3"
-          mx={["2", "8"]}
-          my="10"
-        >
-          <Search />
+        <Suspense>
+          <Flex
+            direction={["column", null, "row"]}
+            align={["start", null, "center"]}
+            justify="space-between"
+            gapY="3"
+            mx={["2", "8"]}
+            my="10"
+          >
+            <Search />
 
-          <FiltersMenu />
-        </Flex>
+            <FiltersMenu />
+          </Flex>
 
-        <CountriesList />
+          <CountriesList />
+        </Suspense>
       </Box>
     </>
   );
